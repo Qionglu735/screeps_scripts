000055fb2779692181c8d4df6874b28a941c1852
阮耕书 <wb.ruangengshu@mesg.corp.netease.com>
2018-12-29 17:09:47 +0800

    add 10000 tick trend

--- screeps-mk2/tool.stat.js
+++ screeps-mk2/tool.stat.js
@@ -19,17 +19,17 @@ var stat = {
         }   
         if(typeof Memory.EnergyTrack == "undefined") {
             Memory.EnergyTrack = [];
-            Memory.Energy10TickSum1 = 0, Memory.Energy100TickSum1 = 0, Memory.Energy500TickSum1 = 0, Memory.Energy1000TickSum1 = 0;
-            Memory.Energy10TickSum2 = 0, Memory.Energy100TickSum2 = 0, Memory.Energy500TickSum2 = 0, Memory.Energy1000TickSum2 = 0;
-            Memory.Energy10TickTrend = 0, Memory.Energy100TickTrend = 0, Memory.Energy500TickTrend = 0, Memory.Energy1000TickTrend = 0;
+            Memory.Energy10TickSum1 = 0, Memory.Energy100TickSum1 = 0, Memory.Energy500TickSum1 = 0, Memory.Energy1000TickSum1 = 0, Memory.Energy10000TickSum1 = 0;
+            Memory.Energy10TickSum2 = 0, Memory.Energy100TickSum2 = 0, Memory.Energy500TickSum2 = 0, Memory.Energy1000TickSum2 = 0, Memory.Energy10000TickSum2 = 0;
+            Memory.Energy10TickTrend = 0, Memory.Energy100TickTrend = 0, Memory.Energy500TickTrend = 0, Memory.Energy1000TickTrend = 0, Memory.Energy10000TickTrend = 0;
         }
         Memory.EnergyTrack.unshift(energy);
-        if(Memory.EnergyTrack.length < 2000 || Game.time % 10000 == 1001) {
-            while(Memory.EnergyTrack.length > 2000) {
+        if(Memory.EnergyTrack.length < 20000 || Game.time % 100000 == 10001) {
+            while(Memory.EnergyTrack.length > 20000) {
                 Memory.EnergyTrack.pop();
             }
-            var sum101 = 0, sum1001 = 0, sum5001 = 0, sum10001 = 0;
-            var sum102 = 0, sum1002 = 0, sum5002 = 0, sum10002 = 0;
+            var sum101 = 0, sum1001 = 0, sum5001 = 0, sum10001 = 0, sum100001;
+            var sum102 = 0, sum1002 = 0, sum5002 = 0, sum10002 = 0, sum100002;
             var i = 0
             while(i < Memory.EnergyTrack.length) {
                 if(0 <= i && i < 10) sum101 += Memory.EnergyTrack[i];
@@ -40,10 +40,12 @@ var stat = {
                 if(500 <= i && i < 1000) sum5002 += Memory.EnergyTrack[i];
                 if(0 <= i && i < 1000) sum10001 += Memory.EnergyTrack[i];
                 if(1000 <= i && i < 2000) sum10002 += Memory.EnergyTrack[i];
+                if(0 <= i && i < 10000) sum100001 += Memory.EnergyTrack[i];
+                if(10000 <= i && i < 20000) sum100002 += Memory.EnergyTrack[i];
                 i += 1;
             }
-            Memory.Energy10TickSum1 = sum101, Memory.Energy100TickSum1 = sum1001, Memory.Energy500TickSum1 = sum5001, Memory.Energy1000TickSum1 = sum10001;
-            Memory.Energy10TickSum2 = sum102, Memory.Energy100TickSum2 = sum1002, Memory.Energy500TickSum2 = sum5002, Memory.Energy1000TickSum2 = sum10002;
+            Memory.Energy10TickSum1 = sum101, Memory.Energy100TickSum1 = sum1001, Memory.Energy500TickSum1 = sum5001, Memory.Energy1000TickSum1 = sum10001, Memory.Energy10000TickSum1 = sum100001;
+            Memory.Energy10TickSum2 = sum102, Memory.Energy100TickSum2 = sum1002, Memory.Energy500TickSum2 = sum5002, Memory.Energy1000TickSum2 = sum10002, Memory.Energy10000TickSum2 = sum100002;
         }
         else {
             Memory.Energy10TickSum1 = Memory.Energy10TickSum1 + Memory.EnergyTrack[0] - Memory.EnergyTrack[10];
@@ -53,17 +55,22 @@ var stat = {
             Memory.Energy500TickSum1 = Memory.Energy500TickSum1 + Memory.EnergyTrack[0] - Memory.EnergyTrack[500];
             Memory.Energy500TickSum2 = Memory.Energy500TickSum2 + Memory.EnergyTrack[500] - Memory.EnergyTrack[1000];
             Memory.Energy1000TickSum1 = Memory.Energy1000TickSum1 + Memory.EnergyTrack[0] - Memory.EnergyTrack[1000];
-            Memory.Energy1000TickSum2 = Memory.Energy1000TickSum2 + Memory.EnergyTrack[1000] - Memory.EnergyTrack.pop();
+            Memory.Energy1000TickSum2 = Memory.Energy1000TickSum2 + Memory.EnergyTrack[1000] - Memory.EnergyTrack[2000];
+            Memory.Energy10000TickSum1 = Memory.Energy10000TickSum1 + Memory.EnergyTrack[0] - Memory.EnergyTrack[10000];
+            Memory.Energy10000TickSum2 = Memory.Energy10000TickSum2 + Memory.EnergyTrack[10000] - Memory.EnergyTrack[20000];
+            Memory.EnergyTrack.pop();
         }
         if(Memory.EnergyTrack.length >= 20) Memory.Energy10TickTrend = (Memory.Energy10TickSum1 - Memory.Energy10TickSum2) / 10;
         if(Memory.EnergyTrack.length >= 200) Memory.Energy100TickTrend = (Memory.Energy100TickSum1 - Memory.Energy100TickSum2) / 100;
         if(Memory.EnergyTrack.length >= 500) Memory.Energy500TickTrend = (Memory.Energy500TickSum1 - Memory.Energy500TickSum2) / 500;
         if(Memory.EnergyTrack.length >= 2000) Memory.Energy1000TickTrend = (Memory.Energy1000TickSum1 - Memory.Energy1000TickSum2) /1000;
+        if(Memory.EnergyTrack.length >= 20000) Memory.Energy10000TickTrend = (Memory.Energy10000TickSum1 - Memory.Energy10000TickSum2) /10000;
         console.log("Energy:" + energy,
                     "10Tick:" + Memory.Energy10TickTrend,
                     "100Tick:" + Memory.Energy100TickTrend,
                     "500Tick:" + Memory.Energy500TickTrend,
-                    "1000Tick:" + Memory.Energy1000TickTrend);
+                    "1000Tick:" + Memory.Energy1000TickTrend,
+                    "10000Tick:" + Memory.Energy10000TickTrend);
         //////////////////////////////////////////////////////
         if(typeof Memory.UsedCpu == "undefined") {
             Memory.UsedCpu = [];
